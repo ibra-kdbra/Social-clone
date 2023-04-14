0eec5cfce2fc9343ae5096f09302b48cb1a78633
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-14 23:24:25 +0300

    modfiy the index in pages folder and Footer comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import axios from "axios";
+
 const Home=()=> {
   return (
   <h1 className="text-3xl underline text-green-800">
     Hello World
   </h1>
   )
+}
+export const getServerSideProps =async ()=>{
+  const response = await axios.get(`http://localhost:3000/api/post`);
+  console.log(response.data.name);
+  return{
+    props:{}
+  }
 }
 export default Home;
