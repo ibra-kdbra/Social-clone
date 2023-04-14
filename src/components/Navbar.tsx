73416f2ece75061280afd31b5997fbf8477b1f44
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-14 23:26:02 +0300

    file for post in api | and the NavBar comp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { AiOutlineLogout } from 'react-icons/ai';
-import { BiSearch } from 'react-icons/bi';
-import { IoMdAdd } from 'react-icons/io';
-import { GoogleLogin, googleLogout  } from '@react-oauth/google';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
-import { createOrGetUser } from '../utils';
-import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from "../utils";
+import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
-      <Link href='/'>
-        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
           <Image
-            className='cursor-pointer'
+            className="cursor-pointer"
             src={Logo}
-            alt='logo'
-            layout='responsive'
+            alt="logo"
           />
         </div>
       </Link>
