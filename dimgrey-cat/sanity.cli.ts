54b39445c0ab302624b218d18262f3ad226bb5e4
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-13 18:30:45 +0300

    delete this trash

--- dimgrey-cat/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '1ja3m8g4',
-    dataset: 'production'
-  }
-})
--- src/utils/client.ts
+++
@@ -1,9 +0,0 @@
-import {createClient} from '@sanity/client';
-
-export const client = createClient({
-  projectId: 'rlxg4qtr',
-  dataset: 'production',
-  apiVersion: '2023-04-10',
-  useCdn: false,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-});
